eaabf5a566c5091f9f79e327c009da8641e8294a
kimkyoungmin97 <kmboy1210@gmail.com>
2026-03-15 21:42:04 +0900

    프로그래머스(평균 구하기) 통과 - 카운트 필요없이 배열길이사용하기 - 강제형변환 사용하기

---
+++ src/main/java/Find_the_average.java
@@ -0,0 +1,16 @@
+
+public class Find_the_average {
+	
+	 public double solution(int[] arr) {
+	        double answer = 0;
+	        int sum = 0;
+	        double count = 0;
+	        for(int i = 0; i < arr.length; ++i) {
+	            count++;
+	            sum += arr[i];
+	        }
+	        answer = sum/count;
+	        return answer;
+	    }
+
+}
